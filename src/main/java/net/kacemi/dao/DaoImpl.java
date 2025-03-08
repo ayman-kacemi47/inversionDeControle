8cb1d15b03599d9d520a54a3a2518b817050c5d1
aymankacemi01 <aymankacemi01@gmail.com>
2025-03-08 12:42:35 +0000

    first commit

---
+++ src/main/java/net/kacemi/dao/DaoImpl.java
@@ -0,0 +1,9 @@
+package net.kacemi.dao;
+
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        return 0;
+    }
+}
---
+++ src/main/java/net/kacemi/dao/IDao.java
@@ -0,0 +1,5 @@
+package net.kacemi.dao;
+
+public interface IDao {
+    double getData();
+}
